53424ccd956faf79a669de8b110f681799931383
Pasha Senko <pashasenko19@gmail.com>
2024-09-04 12:06:30 +0500

    feat: add GallowsInput class with playerInputLetter method

---
+++ src/main/java/backend/academy/GallowsInput.java
@@ -0,0 +1,26 @@
+package backend.academy;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class GallowsInput {
+    private static final Logger LOGGER = Logger.getLogger(GallowsInput.class.getName());
+    private static Scanner scanner;
+
+    public GallowsInput() {
+        scanner = new Scanner(System.in);
+    }
+
+    public char playerInputLetter() {
+        String str;
+        LOGGER.info("Enter letter: ");
+        str = scanner.nextLine();
+
+        // If the input is correct, the cycle ends
+        while (str.length() != 1 || !Character.isLetter(str.charAt(0))) {
+            LOGGER.warning("Invalid input. Please enter a single letter: ");
+            str = scanner.nextLine();
+        }
+        return str.charAt(0);
+    }
+}
