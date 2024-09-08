5fc6ed2c6fbceff6af717dad3b42e5139d27ac8c
Pasha Senko <pashasenko19@gmail.com>
2024-09-08 20:23:43 +0500

    feat(main): implement game initialization and startup

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,22 @@
 package backend.academy;
 
+import java.io.InputStream;
+import java.io.PrintStream;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
+    public static final InputStream IN = System.in;
+    public static final PrintStream OUT = System.out;
+    private final ViewOfTheGallows view = new ViewOfTheGallows(OUT);
+    GallowsInput gallowsInput = new GallowsInput(IN, OUT);
+
     public static void main(String[] args) {
+        view.helloMessage();
+        GameLogic logic = new GameLogic(
+            Dictionary.initializeWord(gallowsInput.getDifficulty(), gallowsInput.getCategory()),
+            view,
+            gallowsInput);
+        logic.run();
     }
 }
