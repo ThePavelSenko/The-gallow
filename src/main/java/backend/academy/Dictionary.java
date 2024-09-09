8d14ffdc845ea2f4da1185ba8032fff0e3908a8a
Pasha Senko <pashasenko19@gmail.com>
2024-09-09 12:48:21 +0500

    refactor(dictionary): remove unused constructors and fix invalid input handling
    - Removed unnecessary constructors and I/O streams that were not needed.
    - Fixed handling of invalid difficulty and category inputs by selecting a random valid option instead of throwing an exception.
    - rRmoved input and output streams, the word constant stringÐ

--- src/main/java/backend/academy/Dictionary.java
+++ src/main/java/backend/academy/Dictionary.java
@@ -1,16 +1,12 @@
 package backend.academy;
 
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class Dictionary {
-    private final String words;
-    static Scanner scanner = new Scanner(System.in);
-    private static final PrintStream OUT = System.out;
 
     // Constants for categories
     private static final String ANIMALS = "animals";
@@ -26,56 +22,54 @@ public class Dictionary {
     private static final List<String> CATEGORIES = Arrays.asList(ANIMALS, FOOD, COLORS);
     private static final List<String> DIFFICULTIES = Arrays.asList(EASY, MEDIUM, HARD);
 
-    private Dictionary() {
-        String df = DIFFICULTIES.get(new Random().nextInt(DIFFICULTIES.size()));
-        String ctg = CATEGORIES.get(new Random().nextInt(CATEGORIES.size()));
-        this.words = initializeWord(df, ctg);
-    }
-
-    private Dictionary(String df) {
-        String ctg = CATEGORIES.get(new Random().nextInt(CATEGORIES.size()));
-        this.words = initializeWord(df, ctg);
-    }
-
-    private Dictionary(String df, String ctg) {
-        this.words = initializeWord(df, ctg);
-    }
-
     // The logic of initialize word
-    public static String initializeWord(String df, String ctg) {
+    public static String initializeWord(String difficulty, String category) throws IllegalArgumentException {
         List<String> words = new ArrayList<>();
         Random random = new Random();
+        String finalDifficulty = difficulty;
+        String finalCategory = category;
 
-        switch (ctg) {
+        if (!DIFFICULTIES.contains(difficulty)) {
+            finalDifficulty = DIFFICULTIES.get(new Random().nextInt(DIFFICULTIES.size()));
+        }
+
+        if (!CATEGORIES.contains(category)) {
+            finalCategory = CATEGORIES.get(new Random().nextInt(CATEGORIES.size()));
+        }
+
+        switch (finalCategory) {
             case ANIMALS:
-                if (df.equals(EASY)) {
+                if (finalDifficulty.equals(EASY)) {
                     words = Arrays.asList("cat", "dog", "pig");
-                } else if (df.equals(MEDIUM)) {
+                } else if (finalDifficulty.equals(MEDIUM)) {
                     words = Arrays.asList("horse", "tiger", "panda");
-                } else if (df.equals(HARD)) {
+                } else if (finalDifficulty.equals(HARD)) {
                     words = Arrays.asList("cheetah", "giraffe", "leopard");
                 }
                 break;
 
             case FOOD:
-                if (df.equals(EASY)) {
+                if (finalDifficulty.equals(EASY)) {
                     words = Arrays.asList("pie", "egg", "ham");
-                } else if (df.equals(MEDIUM)) {
+                } else if (finalDifficulty.equals(MEDIUM)) {
                     words = Arrays.asList("chips", "toast", "pizza");
-                } else if (df.equals(HARD)) {
+                } else if (finalDifficulty.equals(HARD)) {
                     words = Arrays.asList("chicken", "sausage", "pumpkin");
                 }
                 break;
 
             case COLORS:
-                if (df.equals(EASY)) {
+                if (finalDifficulty.equals(EASY)) {
                     words = Arrays.asList("red", "pink", "blue");
-                } else if (df.equals(MEDIUM)) {
+                } else if (finalDifficulty.equals(MEDIUM)) {
                     words = Arrays.asList("black", "white", "green");
-                } else if (df.equals(HARD)) {
+                } else if (finalDifficulty.equals(HARD)) {
                     words = Arrays.asList("orange", "yellow", "purple");
                 }
                 break;
+
+            default:
+                throw new IllegalArgumentException();
         }
 
         return words.get(random.nextInt(words.size()));
