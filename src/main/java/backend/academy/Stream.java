390c554e4169462f0f903ea2f70542b53a66e2ad
Pasha Senko <pashasenko19@gmail.com>
2024-09-13 13:40:28 +0500

    feat(stream): the constant ЫСÐSCANNER een removed from the stream class

--- src/main/java/backend/academy/Stream.java
+++ src/main/java/backend/academy/Stream.java
@@ -2,12 +2,10 @@ package backend.academy;
 
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Stream {
     public static final InputStream IN = System.in;
     public static final PrintStream OUT = System.out;
-    public static final Scanner SCANNER = new Scanner(IN);
 }
