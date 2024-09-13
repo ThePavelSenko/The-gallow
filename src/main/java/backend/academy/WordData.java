73a8d59a4893a806eeddbc9e704530c25ddcbb5a
Pasha Senko <pashasenko19@gmail.com>
2024-09-13 13:48:02 +0500

    fet): add WordData class for storing word and its description
    Added a new  class that holds a word and its description. This class will be used to encapsulate word-related data in the project.

---
+++ src/main/java/backend/academy/WordData.java
@@ -0,0 +1,13 @@
+package backend.academy;
+
+
+public class WordData {
+    public String word;
+    public String description;
+
+    public WordData(String word, String description) {
+        this.word = word;
+        this.description = description;
+    }
+}
+
